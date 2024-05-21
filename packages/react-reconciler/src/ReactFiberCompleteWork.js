pcaronde/react

[Fiber] render boundary in fallback if it contains a new stylesheet during sync update
When we implemented Suspensey CSS we had a heuristic that if the update was sync we would ignore the loading states of any new stylesheets and just do the commit. But for a stylesheet capability to be useful it needs to reliably prevent FOUC and since the stylesheet api is opt-in through precedence we don't have to maintain backaward compat (old stylesheets do not block commit but then nobody really renders them because of FOUC anyway)

This update modifies the logic to put a boundary back into fallback if a sync update would lead to a stylesheet commiting before it loaded.

packages/react-reconciler/src/ReactFiberCompleteWork.js
@@ -128,6 +128,7 @@ import {
   setShallowSuspenseListContext,
   ForceSuspenseFallback,
   setDefaultShallowSuspenseListContext,
+  getSuspenseHandler,
 } from './ReactFiberSuspenseContext';
 import {popHiddenContext} from './ReactFiberHiddenContext';
 import {findFirstSuspended} from './ReactFiberSuspenseComponent';
@@ -534,33 +535,31 @@ function preloadInstanceAndSuspendIfNeeded(
   // loaded yet.
   workInProgress.flags |= MaySuspendCommit;
 
-  // Check if we're rendering at a "non-urgent" priority. This is the same
-  // check that `useDeferredValue` does to determine whether it needs to
-  // defer. This is partly for gradual adoption purposes (i.e. shouldn't start
-  // suspending until you opt in with startTransition or Suspense) but it
-  // also happens to be the desired behavior for the concrete use cases we've
-  // thought of so far, like CSS loading, fonts, images, etc.
-  //
+  // preload the instance if necessary. Even if this is an urgent render there
+  // could be benefits to preloading early.
+  // @TODO we should probably do the preload in begin work
+  const requiredTimeout = preloadInstance(type, props);
+
   // We check the "root" render lanes here rather than the "subtree" render
   // because during a retry or offscreen prerender, the "subtree" render
   // lanes may include additional "base" lanes that were deferred during
   // a previous render.
-  // TODO: We may decide to expose a way to force a fallback even during a
-  // sync update.
   const rootRenderLanes = getWorkInProgressRootRenderLanes();
   if (!includesOnlyNonUrgentLanes(rootRenderLanes)) {
-    // This is an urgent render. Don't suspend or show a fallback. Also,
-    // there's no need to preload, because we're going to commit this
-    // synchronously anyway.
-    // TODO: Could there be benefit to preloading even during a synchronous
-    // render? The main thread will be blocked until the commit phase, but
-    // maybe the browser would be able to start loading off thread anyway?
-    // Likely a micro-optimization either way because typically new content
-    // is loaded during a transition, not an urgent render.
+    // This is an urgent render. If our required timeout is infinite
+    // we suspend to allow the nearest fallback to commit instead.
+    // if the required timeout is finite we treat it like it is zero
+    // and allow the tree to commit as is.
+    if (requiredTimeout === Infinity) {
+      const nearestBoundary = getSuspenseHandler();
+      if (nearestBoundary !== null) {
+        // When there is a suspense boundary we can put into fallback we do
+        // If not we'll commit without waiting for the required resource
+        suspendCommit();
+      }
+    }
   } else {
-    // Preload the instance
-    const isReady = preloadInstance(type, props);
-    if (!isReady) {
+    if (requiredTimeout) {
       if (shouldRemainOnPreviousScreen()) {
         // It's OK to suspend. Mark the fiber so we know to suspend before the
         // commit phase. Then continue rendering.
@@ -588,12 +587,25 @@ function preloadResourceAndSuspendIfNeeded(
 
   workInProgress.flags |= MaySuspendCommit;
 
+  const requiredTimeout = preloadResource(resource);
   const rootRenderLanes = getWorkInProgressRootRenderLanes();
   if (!includesOnlyNonUrgentLanes(rootRenderLanes)) {
-    // This is an urgent render. Don't suspend or show a fallback.
+    // This is an urgent render. If our required timeout is infinite
+    // we suspend to allow the nearest fallback to commit instead.
+    // if the required timeout is finite we treat it like it is zero
+    // and allow the tree to commit as is.
+    if (requiredTimeout === Infinity) {
+      const nearestBoundary = getSuspenseHandler();
+      if (nearestBoundary !== null) {
+        // When there is a suspense boundary we can put into fallback we do
+        // If not we'll commit without waiting for the required resource
+        suspendCommit();
+      }
+    }
   } else {
-    const isReady = preloadResource(resource);
-    if (!isReady) {
+    // This is a transition. If the resource is not ready
+    // we can suspend to allow the nearest fallback to commit instead.
+    if (requiredTimeout) {
       if (shouldRemainOnPreviousScreen()) {
         workInProgress.flags |= ShouldSuspendCommit;
       } else {

packages/react-reconciler/src/ReactFiberWorkLoop.js
@@ -23,6 +23,7 @@ import type {
 } from './ReactFiberTracingMarkerComponent';
 import type {OffscreenInstance} from './ReactFiberActivityComponent';
 import type {RenderTaskFn} from './ReactFiberRootScheduler';
+import type {Resource} from './ReactFiberConfig';
 
 import {
   enableCreateEventHandleAPI,
@@ -75,6 +76,7 @@ import {
   startSuspendingCommit,
   waitForCommitToBeReady,
   preloadInstance,
+  preloadResource,
   supportsHydration,
   setCurrentUpdatePriority,
   getCurrentUpdatePriority,
@@ -2219,9 +2221,13 @@ function renderRootConcurrent(root: FiberRoot, lanes: Lanes) {
             break;
           }
           case SuspendedOnInstanceAndReadyToContinue: {
+            let resource: null | Resource = null;
             switch (workInProgress.tag) {
+              case HostHoistable: {
+                resource = workInProgress.memoizedState;
+              }
+              // intentional fallthrough
               case HostComponent:
-              case HostHoistable:
               case HostSingleton: {
                 // Before unwinding the stack, check one more time if the
                 // instance is ready. It may have loaded when React yielded to
@@ -2232,8 +2238,10 @@ function renderRootConcurrent(root: FiberRoot, lanes: Lanes) {
                 const hostFiber = workInProgress;
                 const type = hostFiber.type;
                 const props = hostFiber.pendingProps;
-                const isReady = preloadInstance(type, props);
-                if (isReady) {
+                const requiredTimeout = resource
+                  ? preloadResource(resource)
+                  : preloadInstance(type, props);
+                if (requiredTimeout === 0) {
                   // The data resolved. Resume the work loop as if nothing
                   // suspended. Unlike when a user component suspends, we don't
                   // have to replay anything because the host fiber

packages/react-reconciler/src/__tests__/ReactSuspenseyCommitPhase-test.js
@@ -44,7 +44,18 @@ describe('ReactSuspenseyCommitPhase', () => {
     return (
       <suspensey-thing
         src={src}
+        timeout={100}
         onLoadStart={() => Scheduler.log(`Image requested [${src}]`)}
+      />
+    );
+  }
+
+  function SuspenseyCSS({src}) {
+    return (
+      <suspensey-thing
+        src={src}
+        timeout={Infinity}
+        onLoadStart={() => Scheduler.log(`CSS requested [${src}]`)}
       />
     );
   }
@@ -113,14 +124,43 @@ describe('ReactSuspenseyCommitPhase', () => {
         </Suspense>,
       );
     });
-    // We intentionally don't preload during an urgent update because the
-    // resource will be inserted synchronously, anyway.
-    // TODO: Maybe we should, though? Could be that the browser is able to start
-    // the preload in background even though the main thread is blocked. Likely
-    // a micro-optimization either way because typically new content is loaded
-    // during a transition, not an urgent render.
-    expect(getSuspenseyThingStatus('A')).toBe(null);
+    assertLog(['Image requested [A]']);
+    expect(getSuspenseyThingStatus('A')).toBe('pending');
     expect(root).toMatchRenderedOutput(<suspensey-thing src="A" />);
+  });
+
+  test('renders fallback during urgent update if required timeout is Infinite', async () => {
+    const root = ReactNoop.createRoot();
+    resolveSuspenseyThing('A');
+    await act(async () => {
+      root.render(
+        <Suspense fallback={<Text text="Loading..." />}>
+          <SuspenseyCSS src="A" />
+        </Suspense>,
+      );
+    });
+    expect(getSuspenseyThingStatus('A')).toBe('fulfilled');
+    expect(root).toMatchRenderedOutput(<suspensey-thing src="A" />);
+
+    await act(async () => {
+      root.render(
+        <Suspense fallback={<Text text="Loading..." />}>
+          <SuspenseyCSS src="B" />
+        </Suspense>,
+      );
+    });
+    assertLog(['CSS requested [B]', 'Loading...']);
+    expect(getSuspenseyThingStatus('B')).toBe('pending');
+    expect(root).toMatchRenderedOutput(
+      <>
+        <suspensey-thing src="A" hidden={true} />
+        Loading...
+      </>,
+    );
+
+    resolveSuspenseyThing('B');
+    expect(getSuspenseyThingStatus('B')).toBe('fulfilled');
+    expect(root).toMatchRenderedOutput(<suspensey-thing src="B" />);
   });
 
   test('an urgent update interrupts a suspended commit', async () => {

psmman/FBR

packages/react-reconciler/src/ReactFiberCompleteWork.js
@@ -152,7 +152,6 @@ import {
   getRenderTargetTime,
   getWorkInProgressTransitions,
   shouldRemainOnPreviousScreen,
-  getWorkInProgressRootRenderLanes,
 } from './ReactFiberWorkLoop';
 import {
   OffscreenLane,
@@ -161,7 +160,6 @@ import {
   includesSomeLane,
   mergeLanes,
   claimNextRetryLane,
-  includesOnlyNonUrgentLanes,
 } from './ReactFiberLane';
 import {resetChildFibers} from './ReactChildFiber';
 import {createScopeInstance} from './ReactFiberScope';
@@ -534,41 +532,15 @@ function preloadInstanceAndSuspendIfNeeded(
   // loaded yet.
   workInProgress.flags |= MaySuspendCommit;
 
-  // Check if we're rendering at a "non-urgent" priority. This is the same
-  // check that `useDeferredValue` does to determine whether it needs to
-  // defer. This is partly for gradual adoption purposes (i.e. shouldn't start
-  // suspending until you opt in with startTransition or Suspense) but it
-  // also happens to be the desired behavior for the concrete use cases we've
-  // thought of so far, like CSS loading, fonts, images, etc.
-  //
-  // We check the "root" render lanes here rather than the "subtree" render
-  // because during a retry or offscreen prerender, the "subtree" render
-  // lanes may include additional "base" lanes that were deferred during
-  // a previous render.
-  // TODO: We may decide to expose a way to force a fallback even during a
-  // sync update.
-  const rootRenderLanes = getWorkInProgressRootRenderLanes();
-  if (!includesOnlyNonUrgentLanes(rootRenderLanes)) {
-    // This is an urgent render. Don't suspend or show a fallback. Also,
-    // there's no need to preload, because we're going to commit this
-    // synchronously anyway.
-    // TODO: Could there be benefit to preloading even during a synchronous
-    // render? The main thread will be blocked until the commit phase, but
-    // maybe the browser would be able to start loading off thread anyway?
-    // Likely a micro-optimization either way because typically new content
-    // is loaded during a transition, not an urgent render.
-  } else {
-    // Preload the instance
-    const isReady = preloadInstance(type, props);
-    if (!isReady) {
-      if (shouldRemainOnPreviousScreen()) {
-        // It's OK to suspend. Mark the fiber so we know to suspend before the
-        // commit phase. Then continue rendering.
-        workInProgress.flags |= ShouldSuspendCommit;
-      } else {
-        // Trigger a fallback rather than block the render.
-        suspendCommit();
-      }
+  // preload the instance if necessary. Even if this is an urgent render there
+  // could be benefits to preloading early.
+  // @TODO we should probably do the preload in begin work
+  const isReady = preloadInstance(type, props);
+  if (!isReady) {
+    if (shouldRemainOnPreviousScreen()) {
+      workInProgress.flags |= ShouldSuspendCommit;
+    } else {
+      suspendCommit();
     }
   }
 }
@@ -588,17 +560,12 @@ function preloadResourceAndSuspendIfNeeded(
 
   workInProgress.flags |= MaySuspendCommit;
 
-  const rootRenderLanes = getWorkInProgressRootRenderLanes();
-  if (!includesOnlyNonUrgentLanes(rootRenderLanes)) {
-    // This is an urgent render. Don't suspend or show a fallback.
-  } else {
-    const isReady = preloadResource(resource);
-    if (!isReady) {
-      if (shouldRemainOnPreviousScreen()) {
-        workInProgress.flags |= ShouldSuspendCommit;
-      } else {
-        suspendCommit();
-      }
+  const isReady = preloadResource(resource);
+  if (!isReady) {
+    if (shouldRemainOnPreviousScreen()) {
+      workInProgress.flags |= ShouldSuspendCommit;
+    } else {
+      suspendCommit();
     }
   }
 }

packages/react-reconciler/src/ReactFiberWorkLoop.js
@@ -23,6 +23,7 @@ import type {
 } from './ReactFiberTracingMarkerComponent';
 import type {OffscreenInstance} from './ReactFiberActivityComponent';
 import type {RenderTaskFn} from './ReactFiberRootScheduler';
+import type {Resource} from './ReactFiberConfig';
 
 import {
   enableCreateEventHandleAPI,
@@ -75,6 +76,7 @@ import {
   startSuspendingCommit,
   waitForCommitToBeReady,
   preloadInstance,
+  preloadResource,
   supportsHydration,
   setCurrentUpdatePriority,
   getCurrentUpdatePriority,
@@ -2219,9 +2221,13 @@ function renderRootConcurrent(root: FiberRoot, lanes: Lanes) {
             break;
           }
           case SuspendedOnInstanceAndReadyToContinue: {
+            let resource: null | Resource = null;
             switch (workInProgress.tag) {
+              case HostHoistable: {
+                resource = workInProgress.memoizedState;
+              }
+              // intentional fallthrough
               case HostComponent:
-              case HostHoistable:
               case HostSingleton: {
                 // Before unwinding the stack, check one more time if the
                 // instance is ready. It may have loaded when React yielded to
@@ -2232,7 +2238,9 @@ function renderRootConcurrent(root: FiberRoot, lanes: Lanes) {
                 const hostFiber = workInProgress;
                 const type = hostFiber.type;
                 const props = hostFiber.pendingProps;
-                const isReady = preloadInstance(type, props);
+                const isReady = resource
+                  ? preloadResource(resource)
+                  : preloadInstance(type, props);
                 if (isReady) {
                   // The data resolved. Resume the work loop as if nothing
                   // suspended. Unlike when a user component suspends, we don't

packages/react-reconciler/src/__tests__/ReactSuspenseyCommitPhase-test.js
@@ -44,6 +44,7 @@ describe('ReactSuspenseyCommitPhase', () => {
     return (
       <suspensey-thing
         src={src}
+        timeout={100}
         onLoadStart={() => Scheduler.log(`Image requested [${src}]`)}
       />
     );
@@ -104,7 +105,49 @@ describe('ReactSuspenseyCommitPhase', () => {
     expect(root).toMatchRenderedOutput(<suspensey-thing src="B" />);
   });
 
-  test('does not suspend commit during urgent update', async () => {
+  test('suspend commit during initial mount at the root', async () => {
+    const root = ReactNoop.createRoot();
+    await act(async () => {
+      startTransition(() => {
+        root.render(<SuspenseyImage src="A" />);
+      });
+    });
+    assertLog(['Image requested [A]']);
+    expect(getSuspenseyThingStatus('A')).toBe('pending');
+    expect(root).toMatchRenderedOutput(null);
+
+    resolveSuspenseyThing('A');
+    expect(getSuspenseyThingStatus('A')).toBe('fulfilled');
+    expect(root).toMatchRenderedOutput(<suspensey-thing src="A" />);
+  });
+
+  test('suspend commit during update at the root', async () => {
+    const root = ReactNoop.createRoot();
+    await act(() => resolveSuspenseyThing('A'));
+    expect(getSuspenseyThingStatus('A')).toBe('fulfilled');
+    expect(root).toMatchRenderedOutput(null);
+    await act(async () => {
+      startTransition(() => {
+        root.render(<SuspenseyImage src="A" />);
+      });
+    });
+    expect(root).toMatchRenderedOutput(<suspensey-thing src="A" />);
+
+    await act(async () => {
+      startTransition(() => {
+        root.render(<SuspenseyImage src="B" />);
+      });
+    });
+    assertLog(['Image requested [B]']);
+    expect(getSuspenseyThingStatus('B')).toBe('pending');
+    expect(root).toMatchRenderedOutput(<suspensey-thing src="A" />);
+
+    resolveSuspenseyThing('B');
+    expect(getSuspenseyThingStatus('B')).toBe('fulfilled');
+    expect(root).toMatchRenderedOutput(<suspensey-thing src="B" />);
+  });
+
+  test('suspend commit during urgent initial mount', async () => {
     const root = ReactNoop.createRoot();
     await act(async () => {
       root.render(
@@ -113,14 +156,87 @@ describe('ReactSuspenseyCommitPhase', () => {
         </Suspense>,
       );
     });
-    // We intentionally don't preload during an urgent update because the
-    // resource will be inserted synchronously, anyway.
-    // TODO: Maybe we should, though? Could be that the browser is able to start
-    // the preload in background even though the main thread is blocked. Likely
-    // a micro-optimization either way because typically new content is loaded
-    // during a transition, not an urgent render.
-    expect(getSuspenseyThingStatus('A')).toBe(null);
+    assertLog(['Image requested [A]', 'Loading...']);
+    expect(getSuspenseyThingStatus('A')).toBe('pending');
+    expect(root).toMatchRenderedOutput('Loading...');
+
+    resolveSuspenseyThing('A');
+    expect(getSuspenseyThingStatus('A')).toBe('fulfilled');
     expect(root).toMatchRenderedOutput(<suspensey-thing src="A" />);
+  });
+
+  test('suspend commit during urgent update', async () => {
+    const root = ReactNoop.createRoot();
+    await act(() => resolveSuspenseyThing('A'));
+    expect(getSuspenseyThingStatus('A')).toBe('fulfilled');
+    await act(async () => {
+      root.render(
+        <Suspense fallback={<Text text="Loading..." />}>
+          <SuspenseyImage src="A" />
+        </Suspense>,
+      );
+    });
+    expect(root).toMatchRenderedOutput(<suspensey-thing src="A" />);
+
+    resolveSuspenseyThing('A');
+    expect(getSuspenseyThingStatus('A')).toBe('fulfilled');
+    expect(root).toMatchRenderedOutput(<suspensey-thing src="A" />);
+
+    await act(async () => {
+      root.render(
+        <Suspense fallback={<Text text="Loading..." />}>
+          <SuspenseyImage src="B" />
+        </Suspense>,
+      );
+    });
+    assertLog(['Image requested [B]', 'Loading...']);
+    expect(getSuspenseyThingStatus('B')).toBe('pending');
+    expect(root).toMatchRenderedOutput(
+      <>
+        <suspensey-thing src="A" hidden={true} />
+        {'Loading...'}
+      </>,
+    );
+
+    resolveSuspenseyThing('B');
+    expect(getSuspenseyThingStatus('B')).toBe('fulfilled');
+    expect(root).toMatchRenderedOutput(<suspensey-thing src="B" />);
+  });
+
+  test('does not suspend commit during urgent initial mount at the root', async () => {
+    const root = ReactNoop.createRoot();
+    await act(async () => {
+      root.render(<SuspenseyImage src="A" />);
+    });
+    assertLog(['Image requested [A]']);
+    expect(getSuspenseyThingStatus('A')).toBe('pending');
+    expect(root).toMatchRenderedOutput(<suspensey-thing src="A" />);
+
+    resolveSuspenseyThing('A');
+    expect(getSuspenseyThingStatus('A')).toBe('fulfilled');
+    expect(root).toMatchRenderedOutput(<suspensey-thing src="A" />);
+  });
+
+  test('does not suspend commit during urgent update at the root', async () => {
+    const root = ReactNoop.createRoot();
+    await act(() => resolveSuspenseyThing('A'));
+    expect(getSuspenseyThingStatus('A')).toBe('fulfilled');
+    expect(root).toMatchRenderedOutput(null);
+    await act(async () => {
+      root.render(<SuspenseyImage src="A" />);
+    });
+    expect(root).toMatchRenderedOutput(<suspensey-thing src="A" />);
+
+    await act(async () => {
+      root.render(<SuspenseyImage src="B" />);
+    });
+    assertLog(['Image requested [B]']);
+    expect(getSuspenseyThingStatus('B')).toBe('pending');
+    expect(root).toMatchRenderedOutput(<suspensey-thing src="B" />);
+
+    resolveSuspenseyThing('B');
+    expect(getSuspenseyThingStatus('B')).toBe('fulfilled');
+    expect(root).toMatchRenderedOutput(<suspensey-thing src="B" />);
   });
 
   test('an urgent update interrupts a suspended commit', async () => {